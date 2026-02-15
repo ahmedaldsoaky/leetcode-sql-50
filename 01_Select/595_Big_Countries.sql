64709d7f4d0fa64a0578b7036316695d7a656340
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-02-15 17:43:04 +0200

    Add query to select big countries by area or population

--- 01_Select/595_Big_Countries.sql
+++ 01_Select/595_Big_Countries.sql
@@ -4,6 +4,9 @@
 
 -- ============================================
 -- Write your solution below
+select name, population, area
+from World
+where area >= 3000000 or population >= 25000000
 -- ============================================
 
 
