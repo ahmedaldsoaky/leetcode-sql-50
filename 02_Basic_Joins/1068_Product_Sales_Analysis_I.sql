a474077f7cfa3a04bd61ce7838d0f1e672dbc7ac
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-02-17 22:44:41 +0200

    Update link to specific LeetCode problem

--- 02_Basic_Joins/1068_Product_Sales_Analysis_I.sql
+++ 02_Basic_Joins/1068_Product_Sales_Analysis_I.sql
@@ -1,9 +1,11 @@
 -- LeetCode Problem: 1068. Product Sales Analysis I
 -- Difficulty: Easy
--- Link: https://leetcode.com/problems/
+-- Link: https://leetcode.com/problems/product-sales-analysis-i
 
 -- ============================================
--- Write your solution below
+select product_name, year, price
+from Sales s join Product p
+on s.product_id = p.product_id
 -- ============================================
 
 
